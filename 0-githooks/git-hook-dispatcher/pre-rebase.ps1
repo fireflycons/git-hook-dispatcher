6ed4f8d16a37ddf62d8383e99f2719508f93db05
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2023-12-04 07:35:45 +0000

    Improve tests

---
+++ 0-githooks/git-hook-dispatcher/pre-rebase.ps1
@@ -0,0 +1,2 @@
+# If PS throws an exception, this should stop git
+throw "Cancelling pre-rebose"
--- main_test.go
+++ main_test.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func mustGetHookBinary() string {
+	// "mock" location of hook binary
+	// such that githooks directory is discovered.
 	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
@@ -15,51 +21,55 @@ func mustGetHookBinary() string {
 	return filepath.Join(wd, "0-githooks", "hook.exe")
 }
 
-func TestProcessPreCommitHook(t *testing.T) {
-	// Hook is found in hook dir for this repo
-	if result, found := processHook(mustGetHookBinary(), "pre-commit"); !found {
-		t.Error("Hook was not found")
-	} else {
-		t.Logf("returned %d", result)
-	}
+func vaildateScriptResult(t *testing.T, hookName string, expectedResult int, shouldBeFound bool) {
+	result, found := processHook(mustGetHookBinary(), hookName)
+
+	require.Equal(t, shouldBeFound, found, func() string {
+		if shouldBeFound && !found {
+			return fmt.Sprintf("%s.ps1 was not found when it should have been.", hookName)
+		}
+		return fmt.Sprintf("%s.ps1 was found when it should not have been.", hookName)
+	}())
+
+	require.Equalf(t, expectedResult, result, "Script exited with %d when %d was expected.", result, expectedResult)
 }
 
-func TestProcessPreApplyPatch(t *testing.T) {
-	// Hook is found in 0-0githooks-shared
-	if result, found := processHook(mustGetHookBinary(), "pre-applypatch"); !found {
-		t.Error("Hook was not found")
-	} else {
-		t.Logf("returned %d", result)
-	}
+func TestProcessPreCommitHookFoundInRepoHooks(t *testing.T) {
+	// Hook is found in hook dir for this repo.
+	vaildateScriptResult(t, "pre-commit", 0, true)
 }
 
-func TestProcessReferenceTransaction(t *testing.T) {
-	// PowerShell script can process data passed on stdin
-	content := []byte("6c6c4afa4352441ea7b1834eac7bc70aee8248ea 37df81edea7f798982b66f4eadac531d3e730c88 HEAD\n6c6c4afa4352441ea7b1834eac7bc70aee8248ea 37df81edea7f798982b66f4eadac531d3e730c88 refs/heads/master\n")
-	tmpfile, err := os.CreateTemp("", "example")
+func TestProcessPreApplyPatchFoundInSharedHooks(t *testing.T) {
+	// Hook is found in 0-githooks-shared.
+	vaildateScriptResult(t, "pre-applypatch", 0, true)
+}
+
+func TestProcessReferenceTransactionHandlesStdin(t *testing.T) {
+	// PowerShell script can process data passed on stdin.
+	//
+	// Wire up a temp file to act as process stdin.
+	tmpfile, err := os.CreateTemp("", "mock-stdin")
 	if err != nil {
 		t.Error(err)
 	}
 
 	defer os.Remove(tmpfile.Name()) // clean up
 
+	content := []byte("6c6c4afa4352441ea7b1834eac7bc70aee8248ea 37df81edea7f798982b66f4eadac531d3e730c88 HEAD\n6c6c4afa4352441ea7b1834eac7bc70aee8248ea 37df81edea7f798982b66f4eadac531d3e730c88 refs/heads/master\n")
 	if _, err := tmpfile.Write(content); err != nil {
 		t.Error(err)
 	}
 
-	if _, err := tmpfile.Seek(0, 0); err != nil {
+	if _, err := tmpfile.Seek(0, io.SeekStart); err != nil {
 		t.Error(err)
 	}
 
 	oldStdin := os.Stdin
-	defer func() { os.Stdin = oldStdin }() // Restore original Stdin
+	defer func() { os.Stdin = oldStdin }() // Restore original Stdin.
 
 	os.Stdin = tmpfile
-	if result, found := processHook(mustGetHookBinary(), "reference-transaction"); !found {
-		t.Error("Hook was not found")
-	} else {
-		t.Logf("returned %d", result)
-	}
+
+	vaildateScriptResult(t, "reference-transaction", 0, true)
 
 	if err := tmpfile.Close(); err != nil {
 		t.Error(err)
@@ -68,13 +78,11 @@ func TestProcessReferenceTransaction(t *testing.T) {
 
 func TestMissingHookScriptExitsWithZero(t *testing.T) {
 	// If a script isn't found, that should not stop git from continuing
-	result, found := processHook(mustGetHookBinary(), "pre-push")
+	vaildateScriptResult(t, "pre-push", 0, false)
+}
 
-	if found {
-		t.Error("Hook script pre-push should no be present in 0-gihooks.")
-	}
-
-	if result != 0 {
-		t.Error("Missing hook should exit with zero")
-	}
+func TestPowerShellThrowReturnsNonZero(t *testing.T) {
+	// If a script (in this case pre-rebase) throws an exception from PowerShell,
+	// then it should stop git by returning 1.
+	vaildateScriptResult(t, "pre-rebase", 1, true)
 }
